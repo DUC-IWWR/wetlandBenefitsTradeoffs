2141d4b19af0107296a0ec6bc7be041ddbafa525
ashpidwerbesky <ashpidwerbesky@gmail.com>
2025-08-06 11:52:33 -0500

    Finished correlation plots to compare original and updated flux data

--- 01.04_wetlandBenefitsTradoffs_fluxCorrectionCheck.R
+++ 01.04_wetlandBenefitsTradoffs_fluxCorrectionCheck.R
@@ -26,7 +26,7 @@ library(ggtext)
 
 data_original <- read.csv("wetland_tradeoff_df_2025-05-02.csv") 
   colnames(data_original) <-  paste0(colnames(data_original), "_original")
-data_updated <- read_excel("AshCheck/wetland_tradeoff_df_2025-07-28.xlsx", na = "NA") 
+data_updated <- read_excel("wetland_tradeoff_df_2025-07-28.xlsx", na = "NA") 
   colnames(data_updated) <-  paste0(colnames(data_updated), "_updated")
 
 # Data summary
@@ -37,27 +37,42 @@ data <- data_original %>%
   full_join(data_updated, by = c("site_id_original" = "site_id_updated"))
 
 
-ggplot(data, aes(pl_gwp_sum_original, pl_gwp_sum_g_updated)) + 
+GWPcheck <- ggplot(data, aes(pl_gwp_sum_original, pl_gwp_sum_g_updated)) + 
   geom_point() + 
   geom_smooth(method = "lm", col = "black", formula = 'y~x')+
   theme_classic(base_size = 16) + 
-  stat_poly_eq(use_label(c("R2")), size = rel(6)) 
+  stat_poly_eq(use_label(c("R2")), size = rel(6)) + 
+  labs(x = "Original GWP (pseudo-log)", y = "Updated GWP (pseudo-log)")
+GWPcheck
 
-
-ggplot(data, aes(pl_flux_co2_original, pl_flux_co2_updated)) + 
+CO2check <- ggplot(data, aes(pl_flux_co2_original, pl_flux_co2_updated)) + 
   geom_point() + 
   geom_smooth(method = "lm", col = "black", formula = 'y~x')+
   theme_classic(base_size = 16) + 
-  stat_poly_eq(use_label(c("R2")), size = rel(6)) 
+  stat_poly_eq(use_label(c("R2")), size = rel(6)) + 
+  labs(x = "Original CO2 flux (pseudo-log)", y = "Updated CO2 flux (pseudo-log)")
+CO2check
 
-ggplot(data, aes(log_flux_ch4_original, log_flux_ch4_updated)) + 
+CH4check <- ggplot(data, aes(log_flux_ch4_original, log_flux_ch4_updated)) + 
   geom_point() + 
   geom_smooth(method = "lm", col = "black", formula = 'y~x')+
   theme_classic(base_size = 16) + 
-  stat_poly_eq(use_label(c("R2")), size = rel(6)) 
+  stat_poly_eq(use_label(c("R2")), size = rel(6))  + 
+  labs(x = "Original CH4 flux (log)", y = "Updated CH4 (log)")
+CH4check
 
-ggplot(data, aes(pl_flux_n2o_original, pl_flux_n2o_updated)) + 
+N2Ocheck <- ggplot(data, aes(pl_flux_n2o_original, pl_flux_n2o_updated)) + 
   geom_point() + 
   geom_smooth(method = "lm", col = "black", formula = 'y~x')+
   theme_classic(base_size = 16) + 
-  stat_poly_eq(use_label(c("R2")), size = rel(6)) 
+  stat_poly_eq(use_label(c("R2")), size = rel(6)) + 
+  labs(x = "Original N2O flux (pseudo-log)", y = "Updated N2O flux (pseudo-log)")
+N2Ocheck 
+
+grid.arrange(GWPcheck, CO2check, CH4check, N2Ocheck)
+
+#Look at "outliers" 
+# CO2: SK-C-10 has a higher flux (1.64) than updated (-0.46)
+View(data %>% select(site_id_original, pl_flux_co2_original, pl_flux_co2_updated) %>% arrange(pl_flux_co2_updated))
+# N2O: AB01 has a higher updated flux (3.42), than original (-1.3)
+View(data %>% select(site_id_original, pl_flux_n2o_original, pl_flux_n2o_updated) %>% arrange(pl_flux_n2o_original))
